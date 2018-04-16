911dbcf87e20807533ab2218c7a106296d710ade
Marshall <38291790+marshmallow10@users.noreply.github.com>
2018-04-16 11:17:40 -0400

    Add files via upload

---
+++ event.cpp
@@ -0,0 +1,36 @@
+#include "event.h"
+
+Event::Event(string name, int time, int val, int sortcount)
+{
+}
+
+void Event::setEventName(string ref)
+{
+}
+
+void Event::setTime(int ref)
+{
+}
+
+void Event::setEventValue(int ref)
+{
+}
+
+void Event::setSortCount(int ref)
+{
+}
+
+int Event::getEventValue()
+{
+	return 0;
+}
+
+int Event::getEventTime()
+{
+	return 0;
+}
+
+int Event::getSortCount()
+{
+	return 0;
+}
---
+++ wire.cpp
@@ -0,0 +1,36 @@
+#include "wire.h"
+
+Wire::Wire(string val, string nm, int index, vector<Gate*> connections)
+{
+}
+
+void Wire::setWireValue(string ref)
+{
+}
+
+void Wire::setWireName(string name)
+{
+}
+
+void Wire::setIndex(int ref)
+{
+}
+
+void Wire::setDrivers(vector<Gate*> ref)
+{
+}
+
+string Wire::getWireValue()
+{
+	return string();
+}
+
+string Wire::getWireName()
+{
+	return string();
+}
+
+int Wire::getIndex()
+{
+	return 0;
+}
