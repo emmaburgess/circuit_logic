82a0628dfad49080837c575c0f8ea1423f6e7b7d
Marshall <38291790+marshmallow10@users.noreply.github.com>
2018-04-20 11:08:59 -0400

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -1,15 +1,29 @@
 #include "event.h"
 
-Event::Event(string name, int time, int val, int sortcount)
+Event::Event(string nameref, int timeref, int valref, int sortcountref)
 {
+	name = nameref;
+	time = timeref;
+	value = valref;
+	sortcount = sortcountref;
+}
+
+Event::Event()
+{
+	name = "";
+	time = -2;
+	value = -2;
+	sortcount = 0;
 }
 
 void Event::setEventName(string ref)
 {
+	name = ref;
 }
 
 void Event::setTime(int ref)
 {
+	time = ref;
 }
 
 void Event::setEventValue(int ref)
@@ -18,6 +32,7 @@ void Event::setEventValue(int ref)
 
 void Event::setSortCount(int ref)
 {
+	sortcount = ref;
 }
 
 int Event::getEventValue()
